303891232d0322e58f9def8db2b43219d7ff7ba8
jey <jrupan@gmail.com>
2019-03-04 00:01:38 -0500

    added method to get images from API

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -7,10 +7,32 @@ import setting from "../setting.json"; // Importing consumer key from local file
 class Gallery extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { data: null }; // initializing data with null
+  }
+
+  /**
+   * Getting images from API and setting state data
+   * @param {number} pageNumber
+   */
+  getImages(pageNumber = 1) {
+    let apiUrl = `https://api.500px.com/v1/photos?feature=popular&image_size=20,2048&page=${pageNumber}&consumer_key=${
+      setting.consumer_key
+    }`;
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data: data });
+      })
+      .catch(err => console.error(this.props.url, err.toString()));
+  }
+
+  componentDidMount() {
+    // Initialize data from API after the component mount
+    this.getImages();
   }
 
   render() {
+    console.log(this.state.data);
     return (
       <div className="container">
         <div className="row">
