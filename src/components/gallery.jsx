0a8c35666f727bdcc20e2dbe12000c39b8625761
jey <jrupan@gmail.com>
2019-03-03 23:38:26 -0500

    renamed app as gallery component

---
+++ src/components/gallery.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+/**
+ * Control entire gallery component
+ */
+class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <h1>Gallery</h1>
+        </div>
+        <div className="row m1">Pagination</div>
+        <div className="row">Images</div>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
